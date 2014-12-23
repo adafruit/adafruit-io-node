54471b4ae8524638ea78bae3ec409500578ae12e
Todd Treece <toddtreece@gmail.com>
2014-12-23 17:48:56 -0500

    log test send example response to console

--- examples/send.js
+++ examples/send.js
@@ -3,12 +3,12 @@ var AIO = require('../index');
 // create an instance
 aio = AIO(process.env.AIO_KEY || 'xxxxxxxxxxxx');
 
-aio.send('Test Send', 'Test Value', function(err, success) {
+aio.send('Test Send Stream', 'Test Value', function(err, data) {
 
   if(err) {
     return console.error(err);
   }
 
-  console.log(success ? 'created stream!' : 'creation failed :(');
+  console.log(data);
 
 });
