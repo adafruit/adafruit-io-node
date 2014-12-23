eaf24e766163389fdbabc10546c7d6188240a817
Todd Treece <toddtreece@gmail.com>
2014-12-23 13:00:25 -0500

    adds feed update example script

---
+++ examples/feeds/update.js
@@ -0,0 +1,22 @@
+var path = require('path'),
+    AIO = require(path.resolve(__dirname, '..', '..', 'index.js'));
+
+// create an instance
+aio = AIO(process.env.AIO_KEY || 'xxxxxxxxxxxx');
+
+// define the data to update
+var update_data = {
+  description: 'Testing updating a description'
+};
+
+// update the description of feed 'Test Feed Two'
+aio.feeds('Test Feed Two').update(update_data, function(err, updated) {
+
+  if(err) {
+    return console.error(err);
+  }
+
+  // log updated feed
+  console.log(updated);
+
+});
