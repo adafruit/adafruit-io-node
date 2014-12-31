30b8dbb82295739b5257cb986f805a0ab34c5f19
Todd Treece <toddtreece@gmail.com>
2014-12-31 18:43:54 -0500

    adds group next, last and previous example scripts

--- examples/groups/previous.js
+++ examples/groups/previous.js
@@ -3,14 +3,14 @@ var AIO = require('../../index');
 // create an instance
 aio = AIO(process.env.AIO_KEY || 'xxxxxxxxxxxx');
 
-// update the value of stream 1
-aio.feeds('Test').streams(1).update(5, function(err, updated) {
+aio.groups('Test').previous(function(err, data) {
 
   if(err) {
     return console.error(err);
   }
 
-  // log updated feed
-  console.log(updated);
+  // log data object
+  console.log(data);
 
 });
+
