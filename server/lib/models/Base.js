2ac0db1f4e8b9fb3b5f277b9ac9702b467591c78
Todd Treece <toddtreece@gmail.com>
2015-09-10 08:14:51 -0400

    convert base model to nedb methods

--- server/lib/models/Base.js
+++ server/lib/models/Base.js
@@ -19,17 +19,19 @@ class Base {
 
     const type = this.type();
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function all_promise(resolve, reject) {
 
-      let data = [];
+      db.find({type: type}, function all_db(err, docs) {
 
-      db.query({type: type})
-        .on('data', function(d) {
-          data.push(d[type]);
-        })
-        .on('end', function() {
-          resolve(data);
-        });
+        if(err)
+          return reject('database error');
+
+        resolve(docs.map(function all_map(doc) {
+          doc[type].id = doc._id;
+          resolve(doc[type]);
+        }));
+
+      });
 
     });
 
@@ -38,26 +40,19 @@ class Base {
   static get(...args) {
 
     const type = this.type(),
-          id_key_name = args.pop();
+          id = args.pop();
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function get_promise(resolve, reject) {
 
-      let data = null,
-          query = {type: type},
-          id = {}, key = {}, name = {};
+      db.findOne(id_query(type, id), function get_db(err, doc) {
 
-      id[`${type}.id`] = id_key_name;
-      key[`${type}.key`] = id_key_name;
-      name[`${type}.name`] = id_key_name;
-      query['$or'] = [id, key, name];
+        if(err)
+          return reject('database error');
 
-      db.query(query)
-        .once('data', function(d) {
-          data = d[type];
-        })
-        .on('end', function() {
-          resolve(data);
-        });
+        doc[type].id = doc._id;
+        resolve(doc[type]);
+
+      });
 
     });
 
@@ -67,20 +62,22 @@ class Base {
 
     const type = this.type();
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function create_promise(resolve, reject) {
 
       const obj = {type: type};
 
-      sent.id = uuid.v1();
+      delete sent.id;
       sent.updated_at = (new Date()).toISOString();
       sent.created_at = sent.updated_at;
       obj[type] = sent;
 
-      db.put(`${type}|${sent.id}`, obj, function(err) {
+      db.insert(obj, function create_db(err, doc) {
 
-        if(err) return reject(err);
+        if(err)
+          return reject('creation failed');
 
-        resolve(sent);
+        doc[type].id = doc._id;
+        resolve(doc[type]);
 
       });
 
@@ -94,18 +91,21 @@ class Base {
           sent = args.pop(),
           id = args.pop();
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function replace_promise(resolve, reject) {
 
       const obj = {type: type};
 
+      delete sent.id;
       sent.updated_at = (new Date()).toISOString();
       obj[type] = sent;
 
-      db.put(`${type}|${sent.id}`, obj, function(err) {
+      db.update(id_query(type, id), obj, function replace_db(err, doc) {
 
-        if(err) return reject(err);
+        if(err)
+          return reject('update failed');
 
-        resolve(sent);
+        doc[type].id = doc._id;
+        resolve(doc[type]);
 
       });
 
@@ -114,5 +114,19 @@ class Base {
   }
 
 }
+
+const id_query = function id_query(type, id_key_name) {
+
+  const query = {type: type},
+        id = {}, key = {}, name = {};
+
+  id[`${type}.id`] = id_key_name;
+  key[`${type}.key`] = id_key_name;
+  name[`${type}.name`] = id_key_name;
+  query['$or'] = [id, key, name];
+
+  return query;
+
+};
 
 exports = module.exports = Base;
