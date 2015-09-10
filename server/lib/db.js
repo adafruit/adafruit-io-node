39234055c4ea661495b53e9f83a2a97d3d414c14
Todd Treece <toddtreece@gmail.com>
2015-09-10 07:33:07 -0400

    switch to nedb to avoid leveldown native compiles

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,16 +1,17 @@
-const levelQuery = require('level-queryengine'),
-      jsonqueryEngine = require('jsonquery-engine'),
-      level = require('level'),
+const nedb = require('nedb'),
       path = require('path');
 
-const db = levelQuery(
-  level(path.join(__dirname, '..', 'db'), {valueEncoding: 'json'})
-);
+const db = ({
+  filename: path.join(__dirname, '..', 'adafruit-io.db')
+});
 
-db.query.use(jsonqueryEngine());
-
-db.ensureIndex('Feed.id');
-db.ensureIndex('Feed.name');
-db.ensureIndex('Feed.key');
+db.ensureIndex({ fieldName: 'type', unique: true, sparse: true });
+db.ensureIndex({ fieldName: 'Feed.id', unique: true, sparse: true });
+db.ensureIndex({ fieldName: 'Feed.key', unique: true, sparse: true });
+db.ensureIndex({ fieldName: 'Feed.name', unique: true, sparse: true });
+db.ensureIndex({ fieldName: 'Data.id', unique: true, sparse: true });
+db.ensureIndex({ fieldName: 'Group.id', unique: true, sparse: true });
+db.ensureIndex({ fieldName: 'Group.key', unique: true, sparse: true });
+db.ensureIndex({ fieldName: 'Group.name', unique: true, sparse: true });
 
 exports = module.exports = db;
