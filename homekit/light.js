9d1a189c92bd2a89dab39e9b4e23a83dda660596
Todd Treece <toddtreece@gmail.com>
2016-01-28 16:01:11 -0500

    working test version of light accessory

--- homekit/light.js
+++ homekit/light.js
@@ -3,47 +3,78 @@
 const Hap = require('hap-nodejs'),
       Accessory = Hap.Accessory,
       Service = Hap.Service,
-      Characteristic = Hap.Characteristic;
+      Characteristic = Hap.Characteristic,
       uuid = Hap.uuid;
 
 class Light extends Accessory {
 
   constructor(name, io) {
 
-    super(name, uuid.generate('adafruit:accessories:light'));
+    super(name, uuid.generate(`adafruit:accessories:light-${name}`));
 
     this.getService(Service.AccessoryInformation)
         .setCharacteristic(Characteristic.Manufacturer, 'Adafruit Industries')
         .setCharacteristic(Characteristic.Model, 'Adafruit IO Light')
         .setCharacteristic(Characteristic.SerialNumber, 'AIOLIGHT01');
 
-    this.name = name;
+    // just call cb on ident
+    this.on('identify', (paired, cb) => cb());
+
+    this._name = name;
     this._io = io;
 
     this._state = {
-      power: false,
-      brightness: 100
+      power: 0,
+      brightness: 100,
+      hue: 255,
+      saturation: 100
     };
 
-    this._stream = this._io.groups.writable(this.name);
+    this._stream = this._io.Groups.writable(this._name);
+
+    let timer = setTimeout(() => {
+      this._writeState();
+    }, 1000);
 
     this._stream.on('data', (group) => {
-      group = JSON.parse(group);
-      this._state = group.feeds;
+
+      if(timer) {
+        clearTimeout(timer);
+        timer = false;
+      }
+
+      group = JSON.parse(group.toString());
+
+      Object.keys(group.feeds).forEach((key) => {
+        if(/^brightness/.test(key))
+          this._state.brightness = parseInt(group.feeds[key]);
+        if(/^power/.test(key))
+          this._state.power = parseInt(group.feeds[key]);
+        if(/^hue/.test(key))
+          this._state.hue = parseInt(group.feeds[key]);
+        if(/^saturation/.test(key))
+          this._state.saturation = parseInt(group.feeds[key]);
+      });
+
     });
 
-    // just call cb on ident
-    this.on('identify', (paired, cb) => cb(); );
+    const service = this.addService(Service.Lightbulb, `Adafruit Light`);
 
-    this.addService(Service.Lightbulb, 'Lightbulb')
-        .getCharacteristic(Characteristic.On)
-        .on('set', this._lightOnSet.bind(this))
-        .on('get', this._lightOnGet.bind(this));
+    service.getCharacteristic(Characteristic.On)
+           .on('set', this._lightOnSet.bind(this))
+           .on('get', this._lightOnGet.bind(this));
 
-    this.addService(Service.Lightbulb, 'Lightbulb')
-        .getCharacteristic(Characteristic.Brightness)
-        .on('set', this._lightBrightnessSet.bind(this))
-        .on('get', this._lightBrightnessGet.bind(this));
+    service.getCharacteristic(Characteristic.Brightness)
+           .on('set', this._lightBrightnessSet.bind(this))
+           .on('get', this._lightBrightnessGet.bind(this));
+
+    service.getCharacteristic(Characteristic.Hue)
+           .on('set', this._lightHueSet.bind(this))
+           .on('get', this._lightHueGet.bind(this));
+
+    service.getCharacteristic(Characteristic.Saturation)
+           .on('set', this._lightSaturationSet.bind(this))
+           .on('get', this._lightSaturationGet.bind(this));
 
   }
 
@@ -52,8 +83,8 @@ class Light extends Accessory {
   }
 
   _lightOnSet(value, cb) {
-    this._state.power = value;
-    this._stream.write(JSON.stringify({ feeds: this._state }));
+    this._state.power = value ? 1 : 0;
+    this._writeState();
     cb();
   }
 
@@ -63,8 +94,36 @@ class Light extends Accessory {
 
   _lightBrightnessSet(value, cb) {
     this._state.brightness = parseInt(value);
-    this._stream.write(JSON.stringify({ feeds: this._state }));
+    this._writeState();
+    cb();
+  }
+  _lightSaturationGet(cb) {
+    cb(null, this._state.saturation);
+  }
+
+  _lightSaturationSet(value, cb) {
+    this._state.saturation = parseInt(value);
+    this._writeState();
     cb();
   }
 
+  _lightHueGet(cb) {
+    cb(null, this._state.hue);
+  }
+
+  _lightHueSet(value, cb) {
+    this._state.hue = parseInt(value);
+    this._writeState();
+    cb();
+  }
+
+  _writeState() {
+    Object.keys(this._state).forEach((key) => {
+      this._state[key] = this._state[key] || 0;
+    });
+    this._stream.write(JSON.stringify({ feeds: this._state }));
+  }
+
 }
+
+exports = module.exports = Light;
